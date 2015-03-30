5c2a2c3ccbda2d850dc6b2ac40bd04265bcdb5c6
Conrad Bessant <c.bessant@qmul.ac.uk>
2015-03-30 17:07:51 +0100

    Command line parameter support added
    Moved parameters from code to command line parameters

--- intercalc.py
+++ intercalc.py
@@ -1,29 +1,39 @@
 # LC-MS Interferent Calculator
 # Developed with Python 2.7.8
 
-# parameters (hard coded for now)
-targetfile     = "test/targets.tsv"
-backgroundfile = "test/background.tsv"
-massaccuracy   = 0.1
-peakwidth      = 1
+# public library imports
+import argparse
 
+# local imports
 from background import *
 from constants import *
 
-print "\nCreating background object ..."
+# define and parse command line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("targetfile", help="input file containing target peptides")
+parser.add_argument("backgroundfile", help="input file containing background peptides")
+parser.add_argument("outputfile", help="output file containing target peptides with interference information")
+parser.add_argument("massaccuracy", help="mass accuracy in Daltons", type=float)
+parser.add_argument("peakwidth", help="width of eluction peaks in hydrophobicity units", type=float)
+parser.add_argument("-v", "--verbose", action="store_true", help="print progress information to console")
+args = parser.parse_args()
+
+if args.verbose:
+  print "\nCreating background object ..."
 
 # create background object by reading interferent file
-b = Background(backgroundfile, massaccuracy, peakwidth)
+b = Background(args.backgroundfile, args.massaccuracy, args.peakwidth, args.verbose)
 
 # open new file (to write results to)
-outfile = open("intercalc.tsv", "w")  # output always called calcinter.tsv
+outfile = open(args.outputfile, "w")  # output always called intercalc.tsv
 
 # open target file (read only)
-infile = open(targetfile, "r")
+infile = open(args.targetfile, "r")
 
 # append interferent probablity column heading
-outfile.write(infile.readline().rstrip()+"\tInteferent probability\n")
-print "Annotating targets with interference data "
+outfile.write(infile.readline().rstrip()+"\tInteferent score\n")
+if args.verbose:
+  print "Annotating targets with interference data "
 
 # step through target peptides one by one and append calcualted interferent probability
 for line in infile:
@@ -32,13 +42,9 @@ for line in infile:
   mass = float(splitline[MOLWEIGHT_COL])
   h = float(splitline[HYDRO_COL])
   # ask background object for peptide-specific interfrence information
-  ptotal = b.interference(mass, h)
+  ptotal = b.interference(mass, h, args.verbose)
   # write peptide line with appended ptotal to new file
   outfile.write(line.rstrip() + "\t" + str(ptotal) + "\n")
 
 infile.close()
 outfile.close()
-
-print b.massaccuracy
-
-input("\nDone. Press the Enter key to exit.")
